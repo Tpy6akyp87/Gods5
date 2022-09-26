2fbab50e839035785c72c21983e6b7a33de6e452
Tpy6akyp87 <kaellakhim@gmail.com>
2022-09-26 14:41:54 +0300

    12

--- Assets/World/Scripts/RegionTileOnMap.cs
+++ Assets/World/Scripts/RegionTileOnMap.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RegionTileOnMap : MonoBehaviour
 {
@@ -13,20 +15,46 @@ public class RegionTileOnMap : MonoBehaviour
     public int levelOfRegion = 3;
     public string typeOfRegion = "Wild";
 
+    public bool mouseOnTile;
+
     public WorldDataHolder dataHolder;
-    //public RegionList regionList;
+    public RegionTileInfo tileInfo;
     void Start()
     {
         dataHolder = FindObjectOfType<WorldDataHolder>();
+        tileInfo = FindObjectOfType<RegionTileInfo>();
         SaveField();
     }
     void Update()
     {
-        
+        if (mouseOnTile)
+        {
+            tileInfo.GetRegionData(idRegion, visitedRegion = false, visitedPoints = 5, numberOfPoints = 10, levelOfRegion = 3, typeOfRegion = "Wild");
+        }
     }
     
     public void SaveField()
     {
         dataHolder.SaveField(idRegion, visitedRegion = false, visitedPoints = 5, numberOfPoints = 10, levelOfRegion = 3, typeOfRegion = "Wild");
     }
+
+    void OnMouseOver()
+    {
+        mouseOnTile = true;
+    }
+
+    void OnMouseExit()
+    {
+        mouseOnTile = false;
+    }
+
+    void OnMouseDown()
+    {
+        SwitchScene("ExploreScene");
+    }
+    public void SwitchScene(string nextscene)
+    {
+        SceneManager.LoadScene(nextscene);
+    }
+
 }
