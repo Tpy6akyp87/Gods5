9ca7d7df3d741172b80445549f085d178ce99a26
Anatoly <kaellakhim@gmail.com>
2022-09-28 00:46:54 +0300

    9

--- Assets/Explorer/Scripts/Deployer.cs
+++ Assets/Explorer/Scripts/Deployer.cs
@@ -17,14 +17,14 @@ public class Deployer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!timeToLoad)
-        {
-            Point newePoint0 = Instantiate(point, new Vector3(1.3f, 0.8f, 0), point.transform.rotation) as Point;
-            Point newePoint1 = Instantiate(point, new Vector3(5.2f, 2.3f, 0), point.transform.rotation) as Point;
-            Point newePoint2 = Instantiate(point, new Vector3(-3.5f, 2.8f, 0), point.transform.rotation) as Point;
-            Point newePoint3 = Instantiate(point, new Vector3(-1.9f, -2.8f, 0), point.transform.rotation) as Point;
-            Explorer newePoint = Instantiate(explorer, new Vector3(-7.0f, -3.0f, 0), explorer.transform.rotation) as Explorer;
-            timeToLoad = true;
-        }
+        //if (!timeToLoad)
+        //{
+        //    Point newePoint0 = Instantiate(point, new Vector3(1.3f, 0.8f, 0), point.transform.rotation) as Point;
+        //    Point newePoint1 = Instantiate(point, new Vector3(5.2f, 2.3f, 0), point.transform.rotation) as Point;
+        //    Point newePoint2 = Instantiate(point, new Vector3(-3.5f, 2.8f, 0), point.transform.rotation) as Point;
+        //    Point newePoint3 = Instantiate(point, new Vector3(-1.9f, -2.8f, 0), point.transform.rotation) as Point;
+        //    Explorer newePoint = Instantiate(explorer, new Vector3(-7.0f, -3.0f, 0), explorer.transform.rotation) as Explorer;
+        //    timeToLoad = true;
+        //}
     }
 }
