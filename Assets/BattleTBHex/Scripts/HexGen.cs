b3c1af3fb6dd700730dca49eaf2b55702c1ddb46
Anatoly <kaellakhim@gmail.com>
2023-02-26 00:21:21 +0200

    9

---
+++ Assets/BattleTBHex/Scripts/HexGen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGen : MonoBehaviour
+{
+    public GameObject playerStart;
+    public GameObject chunk1;
+    public GameObject chunk2;
+    public GameObject EnemyStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStart = Resources.Load<GameObject>("PlayerStart");
+        chunk1 = Resources.Load<GameObject>("1Chunk");
+        chunk2 = Resources.Load<GameObject>("2Chunk");
+        EnemyStart = Resources.Load<GameObject>("EnemyStart");
+
+        GameObject newPlayerStart = Instantiate(playerStart, new Vector3(0f, 0f, 0f), playerStart.transform.rotation) as GameObject;
+        GameObject newchunk1 = Instantiate(chunk1, new Vector3(0f, 0f, 0f), chunk1.transform.rotation) as GameObject;
+        GameObject newchunk2 = Instantiate(chunk2, new Vector3(0f, 0f, 0f), chunk2.transform.rotation) as GameObject;
+        GameObject newEnemyStart = Instantiate(EnemyStart, new Vector3(0f, 0f, 0f), EnemyStart.transform.rotation) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/BattleTBHex/Scripts/HexTile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour
+{
+    public bool empty;
+    public bool unitOnMe;
+    public bool canMoveOnMe;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+    }
+}
